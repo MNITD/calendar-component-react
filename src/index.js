4df2799e3fcce59f6dad0350ca735c26bf7cb3ed
MNITD <bogdanleblank@ukr.net>
2018-04-10 17:36:00 +0300

    minor fix in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calendar from './calendar/calendar.component.js';
+import Calendar from './calendar/calendar.component.js`';
 
 ReactDOM.render(<Calendar/>, document.getElementById('root'));
